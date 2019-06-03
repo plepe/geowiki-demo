fe689f75208edc18db92503c32dfc1947d6fe807
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2019-06-03 21:48:27 +0200

    Save files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 /* global L:false, FileReader:false */
 
+const { saveAs } = require('file-saver')
+
 require('leaflet')
 require('leaflet-geowiki/editor')
 require('leaflet-toolbar')
 
-let OpenControl = L.Control.extend({
+let FileControl = L.Control.extend({
   options: {
     position: 'topleft'
   },
   onAdd (map) {
     let dom = document.createElement('div')
     dom.className = 'leaflet-bar'
-    dom.innerHTML = '<a><label style="width: 100%; height: 100%; display: inline-block;"><input type="file" id="openAction" style="position: fixed; top: -1000px;"><i class="fas fa-folder-open"></i></label></a>'
+    dom.innerHTML = '<a><label style="width: 100%; height: 100%; display: inline-block;"><input type="file" id="openAction" style="position: fixed; top: -1000px;"><i class="fas fa-folder-open"></i></label></a>' +
+      '<a id="downloadAction"><i class="fas fa-download"></i></a>'
     dom.querySelector('#openAction').onchange = e => this.readFile(e)
+    dom.querySelector('#downloadAction').onclick = e => this.options.onsave(e)
     return dom
   },
   // adapted from https://stackoverflow.com/a/26298948
@@ -37,7 +41,7 @@ window.onload = () => {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map)
 
-  new OpenControl({
+  new FileControl({
     onopen (contents) {
       let data = JSON.parse(contents)
 
@@ -45,6 +49,18 @@ window.onload = () => {
         if (err) {
           console.error(err)
         }
+      })
+    },
+    onsave () {
+      let files = geowiki.save()
+      files.forEach(filedata => {
+        let contents = JSON.stringify(filedata.contents, null, '  ')
+
+        let blob = new Blob([ contents ], {
+          type: 'application/vnd.geo+json;charset=utf-8'
+        })
+
+        saveAs(blob, filedata.filename)
       })
     }
   }).addTo(map)
