3822470c592235c5c95fe09f5c1d9e54ba257579
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2019-07-25 08:11:13 +0200

    FileControl: show load/save buttons only when handlers defined

--- src/FileControl.js
+++ src/FileControl.js
@@ -5,10 +5,22 @@ module.exports = L.Control.extend({
   onAdd (map) {
     let dom = document.createElement('div')
     dom.className = 'leaflet-bar'
-    dom.innerHTML = '<a><label style="width: 100%; height: 100%; display: inline-block;"><input type="file" multiple="multiple" id="openAction" style="position: fixed; top: -1000px;"><i class="fas fa-folder-open"></i></label></a>' +
-      '<a id="downloadAction"><i class="fas fa-download"></i></a>'
-    dom.querySelector('#openAction').onchange = e => this.readFile(e)
-    dom.querySelector('#downloadAction').onclick = e => this.options.onsave(e)
+
+    if (this.options.onopen) {
+      let a = document.createElement('a')
+      dom.appendChild(a)
+      a.innerHTML = '<label style="width: 100%; height: 100%; display: inline-block;"><input type="file" multiple="multiple" id="openAction" style="position: fixed; top: -1000px;"><i class="fas fa-folder-open"></i></label>'
+      a.querySelector('#openAction').onchange = e => this.readFile(e)
+    }
+
+    if (this.options.onsave) {
+      let a = document.createElement('a')
+      a.id = 'downloadAction'
+      dom.appendChild(a)
+      a.innerHTML = '<i class="fas fa-download"></i>'
+      a.onclick = e => this.options.onsave(e)
+    }
+
     return dom
   },
   // adapted from https://stackoverflow.com/a/26298948
