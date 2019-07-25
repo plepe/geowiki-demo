b4eaf059d342cf36951e0f481db51d3080900419
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2019-07-25 08:05:18 +0200

    Editor: move FileControl to separate file

---
+++ src/FileControl.js
@@ -0,0 +1,25 @@
+module.exports = L.Control.extend({
+  options: {
+    position: 'topleft'
+  },
+  onAdd (map) {
+    let dom = document.createElement('div')
+    dom.className = 'leaflet-bar'
+    dom.innerHTML = '<a><label style="width: 100%; height: 100%; display: inline-block;"><input type="file" multiple="multiple" id="openAction" style="position: fixed; top: -1000px;"><i class="fas fa-folder-open"></i></label></a>' +
+      '<a id="downloadAction"><i class="fas fa-download"></i></a>'
+    dom.querySelector('#openAction').onchange = e => this.readFile(e)
+    dom.querySelector('#downloadAction').onclick = e => this.options.onsave(e)
+    return dom
+  },
+  // adapted from https://stackoverflow.com/a/26298948
+  readFile (e) {
+    Array.from(e.target.files).forEach(file => {
+      var reader = new FileReader()
+      reader.onload = (e) => {
+        var contents = e.target.result
+        this.options.onopen(file.name, contents)
+      }
+      reader.readAsText(file)
+    })
+  }
+})
